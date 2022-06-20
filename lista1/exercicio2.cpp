00db6c8f98460ea3fdae21001eeb272a1beb1763
aemanuell <antonioemanueell@gmail.com>
2022-06-20 20:10:23 -0300

    last commit

--- lista1/exercicio2.cpp
+++ lista1/exercicio2.cpp
@@ -3,16 +3,38 @@
 using namespace std;
 
 int main() {
-    char caractere, carac;
+    char caractere;
 
     cout << "Digite um carctere ";
     cin >> caractere;
 
-    if(caractere == 'A' || caractere == 'E' || caractere == 'I' || caractere == 'O' || caractere == 'U' || caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u'){
-        cout << "vogal \n";
-    } else {
-        cout << "consoante \n";
-    }
+    switch(caractere)
+        {
+        case 'A':
+        case 'a':
+            cout << "vogal";
+            break;
 
+        case 'E':
+        case 'e':
+            cout << "vogal";
+            break;
+        case 'I':
+        case 'i':
+            cout << "vogal";
+            break;
+
+        case 'O':
+        case 'o':
+            cout << "vogal";
+            break;
+        case 'U':
+        case 'u':
+            cout << "vogal";
+            break;
+        default:
+            printf("Consoante\n");
+        }
+        
     return 0;
 }
